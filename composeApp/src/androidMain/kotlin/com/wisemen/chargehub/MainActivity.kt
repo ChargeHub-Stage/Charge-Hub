3b14ae7694e52eb85f9969abe78fe76d9939e3f0
Yarin18 <91054329+Yarin18@users.noreply.github.com>
2024-03-14 12:54:08 +0100

    remove test code

--- composeApp/src/androidMain/kotlin/com/wisemen/chargehub/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/wisemen/chargehub/MainActivity.kt
@@ -1,16 +1,13 @@
 package com.wisemen.chargehub
 
 import App
-import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.annotation.RequiresApi
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.lifecycleScope
 import db.chargehub.Car
-import db.networking.request.CreateReservationRequest
 import db.repository.car.RemoteCarRepository
 import db.repository.chargehub.RemoteChargeHubRepository
 import db.repository.level.RemoteLevelRepository
@@ -19,11 +16,9 @@ import db.repository.user.RemoteUserRepository
 import kotlinx.coroutines.launch
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
-import java.time.LocalDateTime
 
 class MainActivity : ComponentActivity(), KoinComponent {
 
-    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val userRepo: RemoteUserRepository by inject()
@@ -34,16 +29,6 @@ class MainActivity : ComponentActivity(), KoinComponent {
 
         lifecycleScope.launch {
             val cars = mutableListOf<Car>()
-
-            reservationRepo.create(
-                CreateReservationRequest(
-                    "1",
-                    "1",
-                    "1",
-                    LocalDateTime.now().toString(),
-                    LocalDateTime.now().plusHours(2).toString()
-                )
-            )
 
             carRepo.findAll().collect {
                 cars += it
