992d2ad4621c0bf0e2e7e0819b74257b590cff39
Yarin18 <91054329+Yarin18@users.noreply.github.com>
2024-03-14 09:44:05 +0100

    format CreateUserRequest

--- shared/src/commonMain/kotlin/db/networking/request/CreateUserRequest.kt
+++ shared/src/commonMain/kotlin/db/networking/request/CreateUserRequest.kt
@@ -3,4 +3,10 @@ package db.networking.request
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class CreateUserRequest(val levelId: Long, val name: String, val email: String, val password: String, val currentPoints: Long)
+data class CreateUserRequest(
+    val levelId: Long,
+    val name: String,
+    val email: String,
+    val password: String,
+    val currentPoints: Long
+)
