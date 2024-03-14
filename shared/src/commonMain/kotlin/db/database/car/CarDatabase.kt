5fbfa478e7a32dfd16d39860f07f85fbfbdfbe5a
Yarin18 <91054329+Yarin18@users.noreply.github.com>
2024-03-14 11:24:16 +0100

    Migrate user repository to new generic repository

--- shared/src/commonMain/kotlin/db/database/car/CarDatabase.kt
+++ shared/src/commonMain/kotlin/db/database/car/CarDatabase.kt
@@ -1,14 +1,44 @@
 package db.database.car
 
 import app.cash.sqldelight.db.SqlDriver
-import db.chargehub.ChargeHubDb
-import db.chargehub.User
+import db.chargehub.Car
+import db.chargehub.CarDbQueries
+import db.database.GenericDatabaseOperations
+import db.networking.request.CreateCarRequest
 
-class CarDatabase(sqlDriver: SqlDriver) {
+class CarDatabase(sqlDriver: SqlDriver) :
+    GenericDatabaseOperations<Car, CreateCarRequest>(sqlDriver) {
+    private val query: CarDbQueries
+        get() = database.carDbQueries
 
-    private val database = ChargeHubDb(sqlDriver)
-    private val dbQuery = database.userDbQueries
+    override fun getAll(): List<Car> {
+        return query.getAllCars().executeAsList()
+    }
 
+    override fun getById(id: Long): Car {
+        return query.getCarById(id).executeAsOne()
+    }
+
+    override fun delete(id: Long) {
+        query.deleteCar(id)
+    }
+
+    override fun update(id: Long, request: CreateCarRequest) {
+        query.updateCar(
+            id = id,
+            brand = request.brand,
+            plate = request.plate,
+            userId = request.userId
+        )
+    }
+
+    override fun create(request: CreateCarRequest) {
+        query.insertCar(
+            userId = request.userId,
+            brand = request.brand,
+            plate = request.plate
+        )
+    }
 
 
 }
