80e9f00a3e774fa7cec346771ee1a2a9dcda0fba
Yarin18 <91054329+Yarin18@users.noreply.github.com>
2024-03-14 09:09:03 +0100

    import chage

--- shared/src/iosMain/kotlin/di/PlatformModules.ios.kt
+++ shared/src/iosMain/kotlin/di/PlatformModules.ios.kt
@@ -2,8 +2,8 @@ package di
 
 import app.cash.sqldelight.driver.native.NativeSqliteDriver
 import db.chargehub.ChargeHubDb
-import db.database.UserDatabase
-import db.database.UserDatabaseWrapper
+import db.database.user.UserDatabase
+import db.database.user.UserDatabaseWrapper
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
