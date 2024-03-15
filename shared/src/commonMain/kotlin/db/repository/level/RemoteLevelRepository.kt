053eefa674a1f0ce5b584d249a00ff5a777d90db
Sebastian Van Grieken <r0889358@student.thomasmore.be>
2024-03-15 10:08:53 +0100

    firebase level crud

--- shared/src/commonMain/kotlin/db/repository/level/RemoteLevelRepository.kt
+++ shared/src/commonMain/kotlin/db/repository/level/RemoteLevelRepository.kt
@@ -5,25 +5,48 @@ import db.database.level.LevelDatabase
 import db.database.level.LevelDatabaseWrapper
 import db.networking.request.CreateLevelRequest
 import db.repository.GenericRepository
+import dev.gitlive.firebase.Firebase
+import dev.gitlive.firebase.firestore.firestore
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOf
 import org.koin.core.component.inject
 
-class RemoteLevelRepository : GenericRepository<CreateLevelRequest, Level, LevelDatabase> {
+class RemoteLevelRepository :
+    GenericRepository<CreateLevelRequest, Level, LevelDatabase> {
 
+    private val firestore = Firebase.firestore
     override val database: LevelDatabase
         get() = inject<LevelDatabaseWrapper>().value.database
 
+    override suspend fun create(request: CreateLevelRequest) {
+        firestore.collection("LEVELS").add(request)
+    }
     override suspend fun fetchAll(): List<Level> {
-        return database.getAll()
+        try {
+            val levelResponse =
+                firestore.collection("LEVELS").get()
+            return levelResponse.documents.map { it.data() }
+        } catch (e: Exception) {
+            throw e
+        }
     }
 
     override suspend fun fetchById(id: String): Level {
-       return database.getById(id)
+        try {
+            val levelDocument =
+                firestore.collection("LEVELS").document(id).get()
+            return levelDocument.data()
+        } catch(e: Exception) {
+            throw e
+        }
+    }
+
+    override suspend fun update(id: String, request: CreateLevelRequest) {
+        firestore.collection("LEVELS").document(id).update(request)
     }
 
     override suspend fun delete(id: String) {
-        database.delete(id)
+        firestore.collection("LEVELS").document(id).delete()
     }
 
     override fun findById(id: String): Flow<Level> {
@@ -32,13 +55,5 @@ class RemoteLevelRepository : GenericRepository<CreateLevelRequest, Level, Level
 
     override fun findAll(): Flow<List<Level>> {
         return flowOf(database.getAll())
-    }
-
-    override suspend fun update(id: String, request: CreateLevelRequest) {
-        database.update(id, request)
-    }
-
-    override suspend fun create(request: CreateLevelRequest) {
-        database.create(request)
     }
 }
