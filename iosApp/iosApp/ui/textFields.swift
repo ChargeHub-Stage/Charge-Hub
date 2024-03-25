1ad8c4d93bab5130c000240af12671e2db1f961c
Yarin18 <91054329+Yarin18@users.noreply.github.com>
2024-03-25 09:09:13 +0100

    extract common parts

--- iosApp/iosApp/ui/textFields.swift
+++ iosApp/iosApp/ui/textFields.swift
@@ -6,46 +6,43 @@ struct EditText: View {
     var errorMessage: String?
     var shadowRadius: CGFloat = 10
     var isSecure: Bool = false
+    
     var body: some View {
-        
         VStack(alignment: .leading) {
             if let topLabel = topLabel {
                 Text(topLabel)
             }
-            if (!isSecure) {
-                TextField("Type here", text: $input)
-                    .padding()
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 10)
-                            .stroke(Color.blackPearl, lineWidth: 1)
-                    )
-                if let errorMessage = errorMessage {
-                    HStack {
-                        Image(systemName: "exclamationmark.circle.fill")
-                            .foregroundColor(.red)
-                        Text(errorMessage)
-                            .foregroundColor(.red)
-                    }
-                }
-            } else {
+            if isSecure {
                 SecureField("Type here", text: $input)
-                    .padding()
-                
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 10)
-                            .stroke(Color.black, lineWidth: 1)
-                    )
-                if let errorMessage = errorMessage {
-                    HStack {
-                        Image(systemName: "exclamationmark.circle.fill")
-                            .foregroundColor(.red)
-                        Text(errorMessage)
-                            .foregroundColor(.red)
-                    }
+                    .modifier(InputFieldModifier(isSecure: isSecure))
+            } else {
+                TextField("Type here", text: $input)
+                    .modifier(InputFieldModifier(isSecure: isSecure))
+            }
+            if let errorMessage = errorMessage {
+                HStack {
+                    Image(systemName: "exclamationmark.circle.fill")
+                        .foregroundColor(.red)
+                    Text(errorMessage)
+                        .foregroundColor(.red)
                 }
             }
         }
     }
+}
+
+struct InputFieldModifier: ViewModifier {
+    var isSecure: Bool
+    
+    func body(content: Content) -> some View {
+        content
+            .padding()
+            .overlay(
+                RoundedRectangle(cornerRadius: 10)
+                    .stroke(isSecure ? Color.black : Color.blackPearl, lineWidth: 1)
+            )
+    }
+    
 }
 
 
